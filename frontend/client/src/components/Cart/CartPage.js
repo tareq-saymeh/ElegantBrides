c79944897f16548777abc5f151117ef13a3ae908
tareq <s12112542@stu.najah.edu>
2024-07-11 15:45:20 +0300

    cart page

---
+++ frontend/client/src/components/Cart/CartPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
+import Cart from './Cart'
+
+function CartPage() {
+  return (
+    <div>
+        <Navbar/>
+
+        <Cart/>
+
+        <Footer/>
+    </div>
+  )
+}
+
+export default CartPage
